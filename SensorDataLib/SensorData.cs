f72a0c975867809eb05963701e5bcbbf13e2c9f2
Ekmul <emil377g@edu.easj.dk>
2021-11-04 12:46:25 +0100

    Add project files.

---
+++ SensorDataLib/SensorData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SensorDataLib
+{
+    public class SensorData
+    {
+        public string SensorName { get; set; }
+        public int Speed { get; set; }
+        public string TimeStamp { get; set; }
+
+        public SensorData()
+        {
+        }
+
+        public SensorData(string sensorName, int speed, string timeStamp)
+        {
+            SensorName = sensorName;
+            Speed = speed;
+            TimeStamp = timeStamp;
+        }
+
+        public SensorData(string sensorName, int speed)
+        {
+            SensorName = sensorName;
+            Speed = speed;
+        }
+
+        public override string ToString()
+        {
+            return SensorName + " " + Speed + " " + TimeStamp;
+        }
+    }
+}
